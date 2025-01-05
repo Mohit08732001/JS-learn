91ba531cdd0c9b345e7902f25d335075385082ce
Mohit08732001 <89565402+Mohit08732001@users.noreply.github.com>
2025-01-05 06:38:06 +0000

    Changes: Datatype in JS

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // Treat all JS code as newer version
+
+// alert(3+3) // We are using node not browser engine
+
+console.log(3
+    + 3) // Code readability should be high
+
+let name = "Mohit"
+let age = 23
+let isLoggedIn = false
+
+// number
+// bigint
+// string ==> ""
+// boolean ==> true/false
+// null ==> standalone value
+// undefined
+// symbol ==> unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object

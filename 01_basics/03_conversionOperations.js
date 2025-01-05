1edbdf689ac6a1c3af18db1c001b0540a94781d3
Mohit08732001 <89565402+Mohit08732001@users.noreply.github.com>
2025-01-05 07:14:20 +0000

    Changes: Operations in JS

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,6 +1,6 @@
 let score = true
 
-console.log(typeof score);
+// console.log(typeof score);
 
 let valueInNumber = Number(score)
 
@@ -16,7 +16,31 @@ let valueInNumber = Number(score)
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 -> true, 0 -> false
 // "" -> false,  "mohit" -> true
+
+// *****************************Operations*******************************
+
+let value = 3
+let negValue = -value
+
+console.log(negValue)
+
+let str1 = "hello "
+let str2 = "world"
+let str3 = str1 + str2
+
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "3")
+
+// console.log(+true);
+// console.log(+"")
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2

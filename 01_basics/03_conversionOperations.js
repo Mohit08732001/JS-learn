8b985167e49c1cee869c73b76acfd9ef9ebe4739
Mohit08732001 <89565402+Mohit08732001@users.noreply.github.com>
2025-01-05 06:55:04 +0000

    Changes: Data types conversion

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,22 @@
+let score = true
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+// "33" -> 33
+// "33a" -> NaN
+// null -> 0
+// undefined -> 0
+// true -> 1, false -> 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+
+// 1 -> true, 0 -> false
+// "" -> false,  "mohit" -> true
